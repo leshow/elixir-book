a1d9903adb34da38dcc34e34d6e68ca0cc8c3666
Evan Cameron <cameron.evan@gmail.com>
2016-07-24 17:16:09 -0400

    ex7

---
+++ CallingErlang.exs
@@ -0,0 +1,4 @@
+# calling an Erlang function
+# use :atom notation
+
+:io.format("This number ~3.1f~n", [3.141])
---
+++ ModulesAndFunctions-7.exs
@@ -0,0 +1,28 @@
+
+
+defmodule Erl do
+    def float_to_string do
+        :io.format("~p ~n", [12.12])
+    end
+end
+
+defmodule Env do
+    def get_first do
+        {_, a} = System.get_env
+                    |> Map.to_list
+                    |> List.first
+        a
+    end
+    def get_ext do
+        Path.extname("ModulesAndFunctions-7.exs")
+    end
+    def get_cwd do
+        File.cwd()
+    end
+    def get_first_file do
+        File.cwd!
+            |> File.ls!
+            |> List.first
+            |> Path.extname
+    end
+end
